aba5050b1ebd7d48cd3e9e40ff1f79a5af3ea229
bulgogi03 <60800244+bulgogi03@users.noreply.github.com>
2024-05-01 01:08:33 -0700

    cookies for login

--- server/src/index.js
+++ server/src/index.js
@@ -30,7 +30,7 @@ app.post("/login", async (req, res) => {
         if(users.length ===0) return res.json({message: "User not found"});
         const token = serverClient.createToken(users[0].id);
         const passwordMatch = await bcrypt.compare(password, users[0].hashedPassword);
-        if(passwordMatch){//checks if they match and if they do create the cookies
+        if(passwordMatch){
             res.json({
                 token,
                 firstName: users[0].firstName,
