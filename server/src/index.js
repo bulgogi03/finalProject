bfa805da5bc1eb634cea6039af0eae9a40f7ef8f
bulgogi03 <60800244+bulgogi03@users.noreply.github.com>
2024-05-01 01:08:33 -0700

    cookies for login

--- server/src/index.js
+++ server/src/index.js
@@ -23,7 +23,27 @@ app.post("/createAccount", async (req, res) => {//a route in express
         res.json(error);
     }
 });//this creates an account in stream
-app.post("/login");
+app.post("/login", async (req, res) => {
+    try{
+        const {username, password} = req.body;
+        const {users} = await serverClient.queryUsers({name: username});
+        if(users.length ===0) return res.json({message: "User not found"});
+        const token = serverClient.createToken(users[0].id);
+        const passwordMatch = await bcrypt.compare(password, users[0].hashedPassword);
+        if(passwordMatch){
+            res.json({
+                token,
+                firstName: users[0].firstName,
+                lastName: users[0].lastName,
+                username,
+                userId: users[0].userId,
+            });
+        }
+    }
+    catch (error) {
+        res.json(error);
+    }
+});
 
 app.listen(3001, ()=> {
     console.log("server is running on 3001");
