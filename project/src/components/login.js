b1fb78df02bb80a6311776a4adfa0522c165c7ac
bulgogi03 <60800244+bulgogi03@users.noreply.github.com>
2024-05-01 01:08:33 -0700

    cookies for login

--- project/src/components/login.js
+++ project/src/components/login.js
@@ -1,14 +1,21 @@
 import React, { useState } from 'react';
 import Axios from "axios";
-import Cookies from "universal-cookie"
+import Cookies from "universal-cookie";
 
 function Login() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
+  const cookies = new Cookies();
   const login = () => {
-    console.log('Username:', username);
-    console.log('Password:', password);
+    Axios.post("http://localhost:3001/login", {username, password,}).then(res =>{ //change the route based on server/src/index.js route
+      const {firstName, lastName, username, token, userId} = res.data;//set cookies for them
+      cookies.set("token", token);
+      cookies.set("userId", userId);
+      cookies.set("firstName", firstName);
+      cookies.set("lastName", lastName);
+      cookies.set("username", username);
+    });
   };
 
   return (
