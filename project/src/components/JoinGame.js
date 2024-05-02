90dbc868e3da960d553cd21f7f2b7de674610f44
bulgogi03 <60800244+bulgogi03@users.noreply.github.com>
2024-05-01 18:05:01 -0700

    it works so far

--- project/src/components/JoinGame.js
+++ project/src/components/JoinGame.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
-import {useChatContext} from 'stream-chat-react';
+import {useChatContext, Channel} from 'stream-chat-react';
+import Game from './Game';
 
 function JoinGame() {
   const [opponent, setOpponent] =useState("");
@@ -8,19 +9,25 @@ function JoinGame() {
   const createChannel = async () => {//creates a room for the users
     const response = await client.queryUsers({name: {$eq: opponent}});//queries for a username equal to the one player entered
 
-    if (response.users.length === 0 ){
+    if (response.users.length === 0) {
       alert("User not found");
       return;
     }
+  
     const newChannel = await client.channel("messaging", {
-      members: [client.userID, response.users[0].id],//adds the two people to a channel
+      members: [client.userID, response.users[0].id],
     });
-    await newChannel.watch();//to listen to events and allows server to get events
+  
+    await newChannel.watch();
     setChannel(newChannel);
   };
   return (
     <>
-    {channel ? (<h1>Start</h1>)//condition of if channel is not null it shows start
+    {channel ? (//we need direct access to the channel for the Game so that we can see if both users are present
+      <Channel channel={channel}>
+        <Game channel={channel}/>
+      </Channel>
+      )//condition of if channel is not null it shows start
     :(//if channel is null it shows this
     <div className="joinGame">
       <h4>Create Game</h4>
