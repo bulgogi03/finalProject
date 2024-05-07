9562ec1bab49e50ae7f95973c161ec3918976b0c
bulgogi03 <60800244+bulgogi03@users.noreply.github.com>
2024-05-06 18:52:09 -0700

    changes

--- project/src/components/Board.js
+++ project/src/components/Board.js
@@ -9,36 +9,16 @@
 //         <Box picked={} sign={board[0]}/>
 //         <Box picked={} sign={board[1]}/>
 //         <Box picked={} sign={board[2]}/>
-//         <Box picked={} sign={board[3]}/>
-//         <Box picked={} sign={board[4]}/>
 //       </div>
 //       <div className="row">
 //         <Box picked={} sign={board[5]}/>
 //         <Box picked={} sign={board[6]}/>
 //         <Box picked={} sign={board[7]}/>
-//         <Box picked={} sign={board[8]}/>
-//         <Box picked={} sign={board[9]}/>
 //       </div>
 //       <div className="row">
 //         <Box picked={} sign={board[10]}/>
 //         <Box picked={} sign={board[11]}/>
 //         <Box picked={} sign={board[12]}/>
-//         <Box picked={} sign={board[13]}/>
-//         <Box picked={} sign={board[14]}/>
-//       </div>
-//       <div className="row">
-//         <Box picked={} sign={board[15]}/>
-//         <Box picked={} sign={board[16]}/>
-//         <Box picked={} sign={board[17]}/>
-//         <Box picked={} sign={board[18]}/>
-//         <Box picked={} sign={board[19]}/>
-//       </div>
-//       <div className="row">
-//         <Box picked={} sign={board[20]}/>
-//         <Box picked={} sign={board[21]}/>
-//         <Box picked={} sign={board[22]}/>
-//         <Box picked={} sign={board[23]}/>
-//         <Box picked={} sign={board[24]}/>
 //       </div>
 //     </div>
 //   )
